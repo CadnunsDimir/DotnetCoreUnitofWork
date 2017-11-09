681a86f0152b308b69e9fb00ec5511f98f0293b4
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2017-11-08 22:05:39 -0200

    correcting class name

--- Data/Models/Task.cs
+++ Data/Models/Task.cs
@@ -1,6 +1,6 @@
 namespace DotnetCoreUnitofWork.Data.Models
 {
-    public class Tarefa
+    public class Task
     {
         public int Id { get; set; }
         public string Description { get; set; }
